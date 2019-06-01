eaabe1c68b333e13adc5f71efdfbcdb355949075
rafikbelas <rafik.belas@hotmail.fr>
2019-06-01 22:49:23 +0100

    Added navbar and routes to customers and rentals

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import NavBar from "./components/common/navbar";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={() => <Movies />} />
+          <Route path="/customers" component={() => <Customers />} />
+          <Route path="/rentals" component={() => <Rentals />} />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
